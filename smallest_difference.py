b7bd65edf5cf1544452054084eb5451dafbb5e09
timothyjao <timothy.i.jao@gmail.com>
2019-07-17 17:07:09 -0700

    smallest difference

--- smallest_difference.py
+++ smallest_difference.py
@@ -1,4 +1,27 @@
-# 
+def smallestDifference(arrayOne, arrayTwo):
+    arrayOne = sorted(arrayOne)
+	arrayTwo = sorted(arrayTwo)
+	idx_one = 0
+	idx_two = 0
+	smallest =float("inf")
+	current = float("inf")
+	smallest_pair = []
 	
+	while idx_one < len(arrayOne) and idx_two < len(arrayTwo):
+		first_num = arrayOne[idx_one]
+		second_num = arrayTwo[idx_two]
+		if first_num < second_num:
+			current = second_num - first_num
+			idx_one += 1
+		elif first_num > second_num:
+			current = first_num - second_num
+			idx_two += 1
+		else:
+			return [first_num, second_num]
+		
+		if smallest > current:
+			smallest_pair = [first_num, second_num]
+			smallest = current
 	
-	
+	return smallest_pair
+		
