a7a601078261465948d57ef948da97a587fb501d
timothyjao <timothy.i.jao@gmail.com>
2019-07-24 21:34:23 -0700

    finished permutations

---
+++ permutations.py
@@ -0,0 +1,14 @@
+def getPermutations(array):
+    permutations = []
+    permutationsHelper(0, array, permutations)
+    return permutations
+
+def permutationsHelper(i, array, permutations):
+    if i == len(array)-1:
+        permutations.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            array[i], array[j] = array[j], array[i]
+            permutationsHelper(i+1, array,permutations)
+            array[j], array[i] = array[i], array[j]
+        
