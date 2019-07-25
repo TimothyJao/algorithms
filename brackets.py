f09bb3df2f26d95d8d4c9dfd452f79e4327a66bc
timothyjao <timothy.i.jao@gmail.com>
2019-07-25 14:11:14 -0700

    finish brackets

---
+++ brackets.py
@@ -0,0 +1,16 @@
+def balancedBrackets(string):
+    stack = []
+	for bracket in string:
+		if bracket == "(" or bracket == "[" or bracket == "{":
+			stack.append(bracket)
+		else:
+			if len(stack) == 0: return False
+			pair = stack.pop()
+			if bracket == ")" and pair != "(":
+				return False
+			if bracket == "]" and pair != "[":
+				return False
+			if bracket == "}" and pair != "{":
+				return False
+	
+	return len(stack) == 0
