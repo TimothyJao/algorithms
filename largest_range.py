738f58b55eef33634e151be156576ba57c500a17
timothyjao <timothy.i.jao@gmail.com>
2019-07-29 19:17:49 -0700

    fix identations

--- largest_range.py
+++ largest_range.py
@@ -1,6 +1,6 @@
 def largestRange(array):
     largest = []
-	best = 0
+    best = 0
     hash = {}
     for num in array:
         hash[num] = True
@@ -10,20 +10,20 @@ def largestRange(array):
             continue
         
         hash[num] = False
-		length = 1
+        length = 1
         low = num - 1
         high = num + 1
 		
         while low in hash:
             hash[low] = False
             low -= 1
-        	length += 1
+            length += 1
         while high in hash:
             hash[high] = False
             high += 1
-        	length += 1
+            length += 1
         if length > best:
-			best = length
-			largest = [low+1, high-1]
+            best = length
+            largest = [low+1, high-1]
     
     return largest
