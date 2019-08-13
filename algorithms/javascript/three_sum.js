4b5fb207c2e43500dd3ed0ddf4fc3f71dc3c1076
timothyjao <timothy.i.jao@gmail.com>
2019-08-12 19:02:17 -0700

    finish smallest distance in javascript

--- algorithms/javascript/three_sum.js
+++ algorithms/javascript/three_sum.js
@@ -1,11 +1,22 @@
-function threeNumberSum(array, targetSum){
+function threeNumberSum(array, targetSum) {
+    final = []
+    array.sort((a, b) => { return a - b })
     for(let i = 0; i < array.length; i++){
         array.sort((a,b) => {return a-b})
         let first = array[i];
         left = i + 1;
         right = array.length-1;
         while(left < right){
-        
+            let sum = first + array[left] + array[right]
+            if ( sum === targetSum) { 
+                final.push([first, array[left], array[right]]);
+                left += 1;
+                right -= 1;
+            } else{
+                if(sum > targetSum){right -= 1}
+                else if (sum < targetSum){left += }
+            }
         }
     }
+    return final
 }
