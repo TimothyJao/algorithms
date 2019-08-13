34e1f16d631f09f39575664e6abfbc3cae326bb3
timothyjao <timothy.i.jao@gmail.com>
2019-08-12 18:26:53 -0700

    finish three sum in js

---
+++ algorithms/javascript/three_sum.js
@@ -0,0 +1,11 @@
+function threeNumberSum(array, targetSum){
+    for(let i = 0; i < array.length; i++){
+        array.sort((a,b) => {return a-b})
+        let first = array[i];
+        left = i + 1;
+        right = array.length-1;
+        while(left < right){
+        
+        }
+    }
+}
