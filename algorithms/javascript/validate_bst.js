3231cca2af374432a40738179b545f9e94d4248e
timothyjao <timothy.i.jao@gmail.com>
2019-08-14 14:16:43 -0700

    bst validation in js

---
+++ algorithms/javascript/validate_bst.js
@@ -0,0 +1,9 @@
+function validateBst(tree){
+    return validateBstHelper(tree, -Infinity, Infinity);
+}
+
+function validateBstHelper(tree, minValue, maxValue) {
+    if(tree === null) return true;
+    if(tree.value < minValue || tree.value >= maxValue) return false;
+    return validateBstHelper(tree.left, minValue, tree.value) && validateBstHe(tree.right, tree.value, maxValue);
+}
