397dac2f14ce28bbc6264cab1e95b6191d599946
timothyjao <timothy.i.jao@gmail.com>
2019-08-16 20:21:43 -0700

    fix validate bts

--- algorithms/javascript/validate_bst.js
+++ algorithms/javascript/validate_bst.js
@@ -3,7 +3,7 @@ function validateBst(tree){
 }
 
 function validateBstHelper(tree, minValue, maxValue) {
-    if(tree === null) return true;
+    if(tree == null) return true;
     if(tree.value < minValue || tree.value >= maxValue) return false;
     return validateBstHelper(tree.left, minValue, tree.value) && validateBstHe(tree.right, tree.value, maxValue);
 }
