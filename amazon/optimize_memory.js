d92031d312f3021de26edef4f233f2471d55eb78
timothyjao <timothy.i.jao@gmail.com>
2019-09-06 12:41:15 -0700

    change optimize memory

--- amazon/optimize_memory.js
+++ amazon/optimize_memory.js
@@ -1,20 +1,36 @@
 function optimizeMemory(foregroundTasks, backgroundTasks, memorySpace){
-    let task_hash = {}
-    let task_combos = []
-    let max = 0;
-    
-    for (let i = 0; i < foregroundTasks.length; i++){
-        task_hash[memorySpace-foregroundTasks[i]] = i
-    }
+    foregroundTasks.sort((a,b)=>(a-b))
+    backgroundTasks.sort((a,b)=>(a-b))
 
-    for (let i = 0; i < backgroundTasks.length; i++){
-        if(task_hash[backgroundTasks[i]]){
-            task_combos.push([task_hash[backgroundTasks[i]], i])
+    let fore_pointer = 0;
+    let back_pointer = backgroundTasks.length-1;
+    let max = 0;
+    let results = []
+
+    while (fore_pointer < foregroundTasks.length && back_pointer >= 0){
+        let sum = foregroundTasks[fore_pointer][1] + backgroundTasks[back_pointer][1];
+        if(sum > memorySpace){
+            back_pointer--;
+        } else{
+            if(sum >= max){
+                if (sum > max) {
+                    max = sum;
+                    results = []
+                }
+                results.push([foregroundTasks[fore_pointer][0], backgroundTasks[back_pointer][0]])
+
+                let index = back_pointer - 1;
+                while(index >= 0 && backgroundTasks[index][1] === backgroundTasks[index+1][1]){
+                    results.push([foregroundTasks[fore_pointer][0], backgroundTasks[index][0]])
+                    index--;
+                }
+            }
+            fore_pointer++;
         }
     }
 
-    return task_combos
+    return results
 }
 
-console.log(optimizeMemory([1, 7, 2, 4, 5, 6], [3, 1, 2], 6))
-console.log(optimizeMemory([1, 7, 2, 4, 5, 6], [3, 1, 2], 10))
+console.log(optimizeMemory([[1, 2], [2, 4], [3, 6]], [[1, 2]], 7))
+console.log(optimizeMemory([[1, 3], [2, 5], [3, 7], [4, 10]], [[1, 2], [2, 3], [3, 4], [4, 5]], 10))
