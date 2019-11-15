d9b7249ae6f4a9b07538eeaaead7ce35f4371c6e
timothyjao <timothy.i.jao@gmail.com>
2019-11-15 10:59:58 -0800

    started adding leetcode questions

---
+++ leetcode/1_two_sum.js
@@ -0,0 +1,8 @@
+function twoSum(nums, target) {
+    let hash = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[target - nums[i]] >= 0) return [i, hash[target - nums[i]]].sort()
+        else hash[nums[i]] = i;
+    }
+}
