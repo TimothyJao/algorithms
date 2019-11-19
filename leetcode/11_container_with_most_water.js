a39ce7d7aa4f19a1d37c70a2d338a3ee7d3e722e
timothyjao <timothy.i.jao@gmail.com>
2019-11-18 16:05:26 -0800

    11 container with most water

---
+++ leetcode/11_container_with_most_water.js
@@ -0,0 +1,10 @@
+function maxArea(height){
+    let left = 0, right = height.length-1, max = 0;
+
+    while(left < right){
+        max = Math.max(max, Math.min(height[left], height[right])*(right-left));
+        if(height[left] < height[right]) left++;
+        else right--;
+    }
+    return max;
+}
