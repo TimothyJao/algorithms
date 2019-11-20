7ab878aa39614c07d4b275a51c534451cb5b6f0d
timothyjao <timothy.i.jao@gmail.com>
2019-11-20 14:16:33 -0800

    20 valid parentheses

---
+++ leetcode/20_valid_parentheses.js
@@ -0,0 +1,12 @@
+function isValid(s){
+    let stack = [];
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === '(' || s[i] === '[' || s[i] ==='{') stack.push(s[i])
+        else{
+            if(s[i] === ')' && stack.pop() !== '(') return false;
+            if(s[i] === ']' && stack.pop() !== '[') return false;
+            if(s[i] === '}' && stack.pop() !== '{') return false;
+        }
+    }
+    return stack.length === 0
+}
