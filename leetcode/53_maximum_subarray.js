c86f7715f71dfb25ddd44c28c289b973cd11c1d3
TimothyJao <timothy.i.jao@gmail.com>
2019-12-06 14:18:20 -0800

    5 maximum subarray

--- leetcode/53_maximum_subarray.js
+++ leetcode/53_maximum_subarray.js
@@ -2,5 +2,5 @@ function maxSubArray(nums) {
     for (let i = 1; i < nums.length; i++) {
         nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
     }
-    return Math.max(...nums)
+    return Math.max(...nums);
 }
