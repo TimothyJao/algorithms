76842c649987adcdfdbe09125dd02c1e67c6934d
Timothy Jao <timothy.i.jao@gmail.com>
2019-12-18 14:18:21 -0800

    121 best time to buy and sell stock

---
+++ leetcode/121_best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,12 @@
+function maxProfit(prices){
+    let min = Infinity;
+    let maxProfit = 0;
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        } else if(prices[i] - min > maxProfit){
+            maxProfit = prices[i] - min;
+        }
+    }
+    return maxProfit;
+}
