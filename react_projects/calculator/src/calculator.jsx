f6ef1deb86790414e8c8885bec1951a972daa124
Timothy Jao <timothy.i.jao@gmail.com>
2019-12-19 15:43:15 -0800

    got buttons to work

--- react_projects/calculator/src/calculator.jsx
+++ react_projects/calculator/src/calculator.jsx
@@ -1,9 +1,28 @@
 import React, {useState, useEffect} from 'react';
 
 function Calculator(){
+
+    const [value, setValue] = useState(0);
+    const [prevValue, setPrevValue] = useState(0);
+    const [operator, setOperator] = useState("");
+
     return(
         <div>
-            test
+            <div className="row">
+                <div className="number"> 1 </div>
+                <div className="number"> 2 </div>
+                <div className="number"> 3 </div>
+            </div>
+            <div className="row">
+                <div className="number"> 4 </div>
+                <div className="number"> 5 </div>
+                <div className="number"> 6 </div>
+            </div>
+            <div className="row">
+                <div className="number"> 7 </div>
+                <div className="number"> 8 </div>
+                <div className="number"> 9 </div>
+            </div>
         </div>
     )
 }
