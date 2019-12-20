d68fc88627c58e5345b528a86429b5ba66080faf
Timothy Jao <timothy.i.jao@gmail.com>
2019-12-19 16:24:47 -0800

    setup calculator UI

--- react_projects/calculator/src/calculator.jsx
+++ react_projects/calculator/src/calculator.jsx
@@ -7,7 +7,9 @@ function Calculator(){
     const [operator, setOperator] = useState("");
 
     function changeValue(num){
-        value === '0' ? setValue(num) : setValue(value+num)
+        if (value.length < 13) {
+            value === '0' ? setValue(num) : setValue(value + num)
+        }
     }
 
     function clearValue(num){
@@ -19,27 +21,37 @@ function Calculator(){
     return(
         <div>
             <div className="display"> {value} </div>
+            <div className="buttons">
+                <div className="numbers">
+                    <div className="row">
+                        <div className="button" onClick={() => changeValue('1')}> 1 </div>
+                        <div className="button" onClick={() => changeValue('2')}> 2 </div>
+                        <div className="button" onClick={() => changeValue('3')}> 3 </div>
+                    </div>
+                    <div className="row">
+                        <div className="button" onClick={() => changeValue('4')}> 4 </div>
+                        <div className="button" onClick={() => changeValue('5')}> 5 </div>
+                        <div className="button" onClick={() => changeValue('6')}> 6 </div>
+                    </div>
+                    <div className="row">
+                        <div className="button" onClick={() => changeValue('7')}> 7 </div>
+                        <div className="button" onClick={() => changeValue('8')}> 8 </div>
+                        <div className="button" onClick={() => changeValue('9')}> 9 </div>
+                    </div>
+                    <div className="row">
+                        <div className="button" onClick={clearValue}> Clear </div>
+                        <div className="button" onClick={() => changeValue('0')}> 0 </div>
+                        <div className="button" onClick={() => changeValue('1')}> = </div>
+                    </div>
+                </div>
+                <div className="operatoins">
+                    <div className="button"> + </div>
+                    <div className="button"> - </div>
+                    <div className="button"> * </div>
+                    <div className="button"> = </div>
+                </div>
+            </div>
             
-            <div className="row">
-                <div className="number" onClick={() => changeValue('1')}> 1 </div>
-                <div className="number" onClick={() => changeValue('2')}> 2 </div>
-                <div className="number" onClick={() => changeValue('3')}> 3 </div>
-            </div>
-            <div className="row">
-                <div className="number" onClick={() => changeValue('4')}> 4 </div>
-                <div className="number" onClick={() => changeValue('5')}> 5 </div>
-                <div className="number" onClick={() => changeValue('6')}> 6 </div>
-            </div>
-            <div className="row">
-                <div className="number" onClick={() => changeValue('7')}> 7 </div>
-                <div className="number" onClick={() => changeValue('8')}> 8 </div>
-                <div className="number" onClick={() => changeValue('9')}> 9 </div>
-            </div>
-            <div className="row">
-                <div className="number" onClick={clearValue}> Clear </div>
-                <div className="number" onClick={() => changeValue('0')}> 0 </div>
-                <div className="number" onClick={() => changeValue('1')}> = </div>
-            </div>
         </div>
     )
 }
