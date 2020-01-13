212624c9bdbeac40dfc2c451b81ae58877db57a3
timothyjao <timothy.i.jao@gmail.com>
2020-01-13 15:58:11 -0800

    add draggable back in

--- react_projects/drag_and_drop/src/draggable.jsx
+++ react_projects/drag_and_drop/src/draggable.jsx
@@ -12,7 +12,7 @@ export default class Draggable extends React.Component{
 
     render(){
         return(
-            <div id={this.props.id} onDragStart={this.drag} onDragOver={this.noAllowDrop} style={this.props.style}>
+            <div id={this.props.id} onDragStart={this.drag} onDragOver={this.noAllowDrop} style={this.props.style} draggable={true}>
                 {this.props.children}
             </div>
         );
